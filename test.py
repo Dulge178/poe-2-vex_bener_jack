b80aed313753639fcd803f6909004cd1640e8990
Dulge178 <dulgerbener19@gmail.com>
2026-05-04 09:20:25 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+"""
+
+Name:      test.py
+Coded By:  Bener Dulger && Jack Pellegrini
+Purpose:   Demonstrate how to use GitHub branches
+Date:      5/4/26
+"""
+
+counter = 0
+
+while(counter < 10):
+    print("Time: " + str(counter))
+    counter += 1
