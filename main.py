9cca16a318210644f0c31e0805bc85bbce2b42de
Dulge178 <dulgerbener19@gmail.com>
2026-05-08 09:33:07 -0400

    Implement inertial sensor calibration and testing functions
    Added functions for inertial sensor calibration and testing.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from vex import *
 # Brain should be defined by default
 brain=Brain()
 
-brain.screen.print("Hello V5")
+brain.screen.print("This is where the brain prints to the screen")
 from vex import *
 
 rightMotor = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
@@ -42,15 +42,58 @@ def bump():
   brain.screen.set_cursor(1,1)
   brain.screen.print("Program executed")
   wait(1, SECONDS)
-    
 
-#--------------------define main function-----------------------------#
+
+def intertialCalibration():
+  """
+  1. Calibrate the intertial sensor
+  2. A wait time of 2 seconds is required to calibrate the sensor
+  3. The function is called at the start of the program execution
+  """
+
+
+  brain.screen.clear_screen()
+  brain.screen.set_cursor(1,1)
+  brain.screen.print("Calibrating the inertial sensor")
+  brain.screen.set_cursor(2,1)
+  brain.screen.print("Don't move the robot")
+  inertial_1.calibrate()                    #calibrate the intertial sensor
+
+  wait(2, SECONDS)                   #time required to calibrate the sensor          
+
+  brain.screen.clear_screen()
+  brain.screen.set_cursor(1,1)
+  brain.screen.print("Calibration is complete!")
+
+def testInertial():
+  """
+  1. Test the inertial sensor by having it diplay heading and rotation data.
+  2. Press the button to end the test
+  """
+
+  brain.screen.clear_screen()
+  while (bumpSwitch.pressing() == False):
+    wait(10, MSEC)
+    brain.screen.set_cursor(5, 1)
+    brain.screen.print("Heading: " + str(inertial_1.heading()))
+    brain.screen.set_cursor(6, 1)
+    brain.screen.print("Rotation: " + str(inertial_1.rotation()))
+    brain.screen.set_cursor(8,1)
+    brain.screen.print("Press the button to end the test")
+  
+  brain.screen.clear_screen()
+  brain.screen.set_cursor(0,1)
+  brain.screen.print("Inertial test is complete")
+
+    #--------------------define main function-----------------------------#
 def main():
   """
   This is the function that will be executed by the brain
   """
 
   bump()     #Call bump function to begin the program
+  intertialCalibration()   #calibrate the inertial sensor
+  testInertial()   #test the inertial heading and rotation
 
 main()
 
