297fd296605d0c7f316af5a94b533233675cd1f3
Dulge178 <dulgerbener19@gmail.com>
2026-05-15 09:48:16 -0400

    Implement driveStraight and driveStraightData functions
    Added functions for driving straight and reporting data.

--- main.py
+++ main.py
@@ -85,6 +85,102 @@ def testInertial():
   brain.screen.set_cursor(0,1)
   brain.screen.print("Inertial test is complete")
 
+def driveStraightData(e):
+  """
+  1. Report position, rotation, and error
+  2. Parameter: 3 = error value (setpoint - rotation)
+  """
+  brain.screen.set_cursor(1,1)
+  brain.screen.print("Position: " + str(leftMotor.position())) # returns position
+
+  brain.screen.set_cursor(2,1)
+  brain.screen.print("Rotation: " + str(inertial_1.rotation())) # returns rotation
+
+  brain.screen.set_cursor(3,1)
+  brain.screen.print("Error: " + str(e)) # error value (setpoint - rotation)
+
+def stopMotors():
+  rightMotor.stop()
+  leftMotor.stop()
+  wait(0.5, SECONDS)
+
+def driveStraight(distance, setpoint, motorVelocity):
+  """
+  1. distanc = distance in inches
+  2. setpoint = 0-deg for driving straight
+  3. motorVelocity = nominal velocity (+) => Forward, (-) => Backward
+  """
+
+  inertial_1.reset_rotation() #Reset the rotation value before moving
+
+  kP = 0.25 # Proportional constant for driving straight
+            # Used to calibrate the velocity correction term
+            # if too small the correction will occur too slowly
+            # if too large the system will over correct
+            # determine the best by iteratively testing 
+  
+  # Calculate the distance in terms of encoder count
+  wheelDiameter = 4 # Units = inches
+  wheelCircumfrence = wheelDiameter * math.pi # Units = inches
+
+  #distance(ticks) = (distance(inches) / Wheel Circumfrence) * 100
+  distance = (distance/wheelCircumfrence) * 360 # units = ticks
+
+
+  # Reset the motor encoders
+  leftMotor.set_position(0, DEGREES)
+  rightMotor.set_position(0, DEGREES)
+
+
+  # Drive straight forward if motor velocity > 0
+  if(motorVelocity > 0):
+    # Use a while loop track the distance traveled
+    while(leftMotor.position() < distance):
+      error = (setpoint - inertial_1.rotation()) # Rotation error
+      correction = kP * error # Motor velocity correction term
+
+      # Correct the motor velocities to maintain course
+      # If error > 0 => drift left
+      # If error < 0 => drift right
+
+
+      leftMotor.set_velocity((motorVelocity + correction), PERCENT)
+      rightMotor.set_velocity((motorVelocity - correction), PERCENT)
+
+      #Spin the motors
+      leftMotor.spin(FORWARD)
+      rightMotor.spin(FORWARD)
+    
+      driveStraightData(error) # Display current position, rotation, & error
+
+    stopMotors() # Stop both motors when the desired distance is traveled
+                 # You will need to account for momentum
+
+  else: 
+    distance *= -1 # Distance count must be negative for driving reverse
+    # Use a while loop track the distance traveled
+    while(leftMotor.position() > distance):
+      error = (setpoint - inertial_1.rotation())  # Rotation Error
+      correction = kP * error # Motor velocity correction terms
+
+      # Correct the motor velocities to maintain course
+      # If error > 0 => drift left
+      # If error < 0 => drift right
+
+      leftMotor.set_velocity((motorVelocity + correction), PERCENT)
+      rightMotor.set_velocity((motorVelocity - correction), PERCENT)
+
+      #Spin the motors
+      leftMotor.spin(REVERSE)
+      rightMotor.spin(REVERSE)
+
+      driveStraightData(error) # Display current position, rotation & error
+
+    stopMotors() # Stop both motors when the desired distance is traveled
+                 # You will need to account for momentum
+
+
+
     #--------------------define main function-----------------------------#
 def main():
   """
@@ -92,8 +188,12 @@ def main():
   """
 
   bump()     #Call bump function to begin the program
+  #set stopping value  for left and right motors
+  leftMotor.set_stopping(COAST)
+  rightMotor.set_stopping(COAST)
   intertialCalibration()   #calibrate the inertial sensor
   testInertial()   #test the inertial heading and rotation
 
+  driveStraight(90, 0, 50) # TUne your KP Value
+  
 main()
-
